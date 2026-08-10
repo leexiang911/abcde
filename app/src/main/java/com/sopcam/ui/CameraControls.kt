7e09e10bd8627ef79a0696cda8f7d34bbfdb6863
lixiang <51419474@qq.com>
2026-08-10 12:49:32 +0800

    import detectTransformGestures properly

--- app/src/main/java/com/sopcam/ui/CameraControls.kt
+++ app/src/main/java/com/sopcam/ui/CameraControls.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -223,7 +224,7 @@ fun Modifier.zoomGestures(
     onDoubleTap: () -> Unit,
 ): Modifier = this
     .pointerInput(Unit) {
-        androidx.compose.foundation.gestures.detectTransformGestures { _, _, zoom, _ ->
+        detectTransformGestures { _, _, zoom, _ ->
             if (zoom != 1f) onPinch(zoom)
         }
     }
